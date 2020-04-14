f2915cf7df288f63c578776a42f040dacc8e743e
Tomonari Takai <takai01010100@gmail.com>
2020-04-14 17:23:29 +0900

    HTML to PUG: pages index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,22 @@
-<template>
-  <div :class="$style.container">
-    <div>
-      <TheLogo />
-      <h1 :class="$style.title" v-text="'NuxtStarter'" />
-      <h2 :class="$style.subtitle" v-text="'My excellent Nuxt.js project'" />
-      <div :class="$style.links">
-        <BaseButton
+<template lang="pug">
+  div(:class="$style.container")
+    div
+      TheLogo
+      h1(:class="$style.title") NuxtStarter
+      h2(:class="$style.subtitle") My excellent Nuxt.js project
+      div(:class="$style.links")
+        BaseButton(
           link-url="https://nuxtjs.org/"
           button-text="Documentation"
           color-type="green"
           target="_blank"
-        />
-        <BaseButton
+        )
+        BaseButton(
           link-url="https://github.com/nuxt/nuxt.js"
           button-text="GitHub"
           color-type="grey"
           target="_blank"
-        />
-      </div>
-    </div>
-  </div>
+        )
 </template>
 
 <script lang="ts" src="./index.ts"></script>
