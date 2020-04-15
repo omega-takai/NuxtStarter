9f463be43383fbcbf069f0d674e4ec30153f39e7
Tomonari Takai <takai01010100@gmail.com>
2020-04-15 13:51:27 +0900

    add stylelint plugins
    https://stylelint.io/user-guide/configure
    https://github.com/stylelint/awesome-stylelint#configs
    https://toragramming.com/programming/nuxt-js/nuxt-stylelint-prettier-vscode-format-scss-on-save/#Stylelint
    https://github.com/tigrig29/nuxt-typescript-template-TigRig/blob/master/package.json

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-recommended-scss',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: [],
   rules: {},
 }
