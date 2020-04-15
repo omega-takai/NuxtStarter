7242847bf705d91df1aaedf50839ccbfc22497a4
Tomonari Takai <takai01010100@gmail.com>
2020-04-15 18:32:38 +0900

    UPDATE style lint rule

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,15 +1,24 @@
+/**
+ * kebab-lowerCase
+ * https://regexper.com/#%5E%28%5Ba-z%5D%29%2B%28%5Ba-zA-Z0-9%5D%29*%28-%3F%5Ba-z%5D%2B%5Ba-zA-Z0-9%5D%29*%24
+ */
+const regexPattern = '^([a-z])+([a-zA-Z0-9])*(-?[a-z]+[a-zA-Z0-9])*$'
+
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   extends: [
-    'stylelint-config-sass-guidelines',
     'stylelint-config-standard',
     'stylelint-config-recess-order',
     'stylelint-config-recommended-scss',
     'stylelint-prettier/recommended',
   ],
   plugins: [],
-  rules: {},
+  rules: {
+    'scss/dollar-variable-pattern': regexPattern,
+    'scss/at-function-pattern': regexPattern,
+    'scss/at-mixin-pattern': regexPattern,
+  },
   // stylelint ignores the node_modules directory by default.
   ignoreFiles: ['**/*.{js,ts}'],
 }
