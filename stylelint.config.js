284bc962302fb37c6f6bfc48aadf9c6b72e65977
Tomonari Takai <takai01010100@gmail.com>
2020-04-15 14:54:24 +0900

    $ yarn add -D stylelint-config-sass-guidelines

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   extends: [
+    'stylelint-config-sass-guidelines',
     'stylelint-config-standard',
     'stylelint-config-recess-order',
     'stylelint-config-recommended-scss',
