04d322b13319330d177060fe04391d34245e91b1
Tomonari Takai <takai01010100@gmail.com>
2020-08-23 16:34:38 +0900

    collectCoverage to false

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ module.exports = {
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest',
   },
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     '<rootDir>/src/components/**/*.vue',
     '<rootDir>/src/pages/**/*.vue',
