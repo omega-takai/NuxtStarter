11b128e97240332a8b6fb538d1a1abb44641cbae
TAKAI Tomonari <takai01010100@gmail.com>
2022-10-11 03:21:54 +0900

    add stylelint-config-recess-order

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,6 +2,7 @@ module.exports = {
   customSyntax: 'postcss-html',
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-recess-order',
     'stylelint-config-recommended-vue',
     'stylelint-config-prettier',
   ],
